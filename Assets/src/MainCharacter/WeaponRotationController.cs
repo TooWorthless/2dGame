b950739f67faa8525e98fb4c697b066c4b9523a8
TooWorthless <91129167+TooWorthless@users.noreply.github.com>
2024-01-03 23:46:35 +0300

    New mob, healing pots, fixes

--- Assets/src/MainCharacter/WeaponRotationController.cs
+++ Assets/src/MainCharacter/WeaponRotationController.cs
@@ -6,6 +6,7 @@ public class WeaponRotationController : MonoBehaviour
 {
     public float offset;
     public PlayerController player;
+    public GameObject weapon;
 
 
     private bool isFliped = false;
@@ -14,16 +15,20 @@ public class WeaponRotationController : MonoBehaviour
     void Update()
     {
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        Debug.Log(Input.mousePosition);
+        // Debug.Log(Input.mousePosition);
         float rotateZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotateZ + offset);
 
         Vector3 localScale = transform.localScale;
 
-        if(rotateZ > 90 || rotateZ < -90) {
+        if(rotateZ >= 90 || rotateZ <= -90) {
             if(!isFliped) {
                 localScale.x *= -1f;
                 transform.localScale = localScale;
+
+                Vector3 weaponScale = weapon.transform.localScale;
+                weaponScale.x *= -1f;
+                weapon.transform.localScale = weaponScale;
 
                 player.Flip2();
                 isFliped = true;
@@ -33,6 +38,10 @@ public class WeaponRotationController : MonoBehaviour
             if(isFliped) {
                 localScale.x *= -1f;
                 transform.localScale = localScale;
+
+                Vector3 weaponScale = weapon.transform.localScale;
+                weaponScale.x *= -1f;
+                weapon.transform.localScale = weaponScale;
 
                 player.Flip2();
                 isFliped = false;
